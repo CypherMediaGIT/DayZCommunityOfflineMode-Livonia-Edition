deab570c6e17e8cc3e377ed15ded32dbfe052973
Cypher Media <futuhdware@zoho.com>
2020-09-28 13:13:38 +0100

    cleanup

---
+++ Missions/DayZCommunityOfflineMode.Enoch/core/BaseModuleInclude.c
@@ -0,0 +1,5 @@
+#include "$CurrentDir:missions\\DayZCommunityOfflineMode.enoch\\core\\ModuleManager.c"
+#include "$CurrentDir:missions\\DayZCommunityOfflineMode.enoch\\core\\StaticFunctions.c"
+
+#include "$CurrentDir:missions\\DayZCommunityOfflineMode.enoch\\core\\CommunityOfflineClient.c"
+#include "$CurrentDir:missions\\DayZCommunityOfflineMode.enoch\\core\\CommunityOfflineServer.c"
